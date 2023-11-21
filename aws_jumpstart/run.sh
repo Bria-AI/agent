4624f40280d55e0343f446fe68d3d05634cd0961
Itay Amir <itay@bria.ai>
2023-11-21 15:00:53 +0200

    push latest version

--- aws_jumpstart/run.sh
+++ aws_jumpstart/run.sh
@@ -7,8 +7,34 @@ template_file="master.yml"
 
 # Step 1: Sync current folder to S3
 echo "Uploading current folder to S3 bucket $bucket_name..."
-aws s3 sync . s3://$bucket_name/
-zip -r lambdas/api_endpoint_lambda/endpoint_lambda_code.zip lambdas/api_endpoint_lambda/endpoint_lambda_code.py
+python3 -m venv venv
+source venv/bin/activate
+
+# create zip dir
+mkdir lambdas/api_endpoint_lambda/build_zip
+
+# copy code base to zip dir
+cp -r lambdas/api_endpoint_lambda/code/ lambdas/api_endpoint_lambda/build_zip/
+python3 -m pip install --upgrade pip
+pip install sagemaker --no-cache-dir
+pip install rpds --no-cache-dir
+pip install rpds-py==0.5.3 --no-cache-dir
+pip install urllib3==1.26.6 --no-cache-dir
+cp -r venv/lib/python3.11/site-packages/* lambdas/api_endpoint_lambda/build_zip/
+
+# create zip
+cd lambdas/api_endpoint_lambda/build_zip/
+zip -r ../endpoint_lambda_code.zip .
+
+# return to base dir
+cd -
+
+# remove venv
+rm -rf venv
+rm -rf lambdas/api_endpoint_lambda/build_zip/
+
+# copy to s3
+aws s3 sync . s3://$bucket_name/ --exclude "*.py" --exclude "*.pyc"
 # connect to ecr repo for pulling docker image
 # aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 785573368785.dkr.ecr.us-east-1.amazonaws.com
 
