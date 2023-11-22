e06ef40c345d824d80e783b18c788c792360b58b
Claudio Vellage <claudio.vellage@pm.me>
2023-11-22 13:13:25 +0100

    Ignore .zip files; update run.sh, add example for config

--- aws_jumpstart/run.sh
+++ aws_jumpstart/run.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-# Variables
-bucket_name="jumpstart-bria-agent"
-stack_name="jumpstart-bria-agent"
-template_file="master.yml"
+# Assign command line arguments to variables
+stack_name=${1:-jumpstart-bria-agent}
+bucket_name=${2:-jumpstart-bria-agent}
+template_file=${3:-master.yml}
+config_file_name=${4:-config.json}
+
+# Check if required parameters are provided
+if [ -z "$bucket_name" ] || [ -z "$stack_name" ]; then
+    echo "Usage: $0 [stack_name] [bucket_name] [template_file]"
+    echo "bucket_name and stack_name are required."
+    echo "template_file is optional and defaults to 'master.yml'."
+    exit 1
+fi
+
+config_file=""
+if [ -f "$config_file_name" ]; then
+    config_file="--parameters file://$config_file_name"
+fi
 
 # Step 1: Sync current folder to S3
 echo "Uploading current folder to S3 bucket $bucket_name..."
@@ -35,15 +49,66 @@ rm -rf lambdas/api_endpoint_lambda/build_zip/
 
 # copy to s3
 aws s3 sync . s3://$bucket_name/ --exclude "*.py" --exclude "*.pyc"
-# connect to ecr repo for pulling docker image
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 785573368785.dkr.ecr.us-east-1.amazonaws.com
 
 # Step 2: Update CloudFormation Stack
 echo "Updating CloudFormation stack $stack_name..."
-# aws cloudformation update-stack --stack-name $stack_name --template-body file://master.yml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND  --role-arn arn:aws:iam::300465780738:role/bria-agent
-aws cloudformation update-stack --stack-name $stack_name --template-body file://master.yml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
-# aws cloudformation create-stack --stack-name $stack_name  --template-body file://master.yml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND --role-arn arn:aws:iam::300465780738:role/bria-agent 
-#   --user-ARN arn:aws:iam::300465780738:user/Itay
 
-echo "Update process initiated. Check the AWS CloudFormation console for progress."
+function stack_exists() {
+    aws cloudformation describe-stacks --stack-name $stack_name > /dev/null 2>&1
+    return $?
+}
+
+function stack_failed() {
+    local stack_status=$(aws cloudformation describe-stacks --stack-name $stack_name --query "Stacks[0].StackStatus" --output text 2> /dev/null)
+    local status
+
+    case $stack_status in
+    "ROLLBACK_COMPLETE")
+	status=0
+        ;;
+    *)
+	status=1
+	;;
+    esac
+
+    return $status
+}
+
+function create_stack() {
+    echo "Creating stack $stack_name"
+    aws cloudformation create-stack --stack-name $stack_name --template-body file://master.yml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND $config_file
+}
+
+function update_stack() {
+    echo "Updating stack $stack_name"
+    aws cloudformation update-stack --stack-name $stack_name --template-body file://master.yml --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND $config_file
+}
+
+function delete_stack() {
+    echo "Deleting stack $stack_name"
+    aws cloudformation delete-stack --stack-name $stack_name
+}
+
+function deploy() {
+    if stack_exists; then
+	if stack_failed; then
+	    delete_stack
+	    create_stack
+	else
+	    update_stack
+	fi
+    else
+	create_stack
+    fi
+
+    return $?
+}
+
+if deploy; then
+    echo "Update process initiated. Check the AWS CloudFormation console for progress."
+else
+    echo "Update process failed."
+fi
+
+
 
