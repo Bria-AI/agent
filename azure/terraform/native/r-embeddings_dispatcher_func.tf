f32649f0a31bc88fc4a692d5dd370131fb60469a
or <or@2bcloud.io>
2024-04-06 23:26:51 +0300

    Change the name

--- azure/terraform/native/r-embeddings_dispatcher_func.tf
+++ azure/terraform/native/r-embeddings_dispatcher_func.tf
@@ -53,16 +53,9 @@ module "embeddings_dispatcher_func" {
   extra_tags = merge(local.extra_tags, {})
 }
 
-# Storage permissions
-resource "azurerm_role_assignment" "sa_image_uploader_data_contributor_embeddings_dispatcher_func" {
-  principal_id         = module.embeddings_dispatcher_func.function_app_identity.principal_id
-  scope                = module.image_uploader_storage_account.storage_account_id
-  role_definition_name = "Storage Blob Data Contributor"
-}
-
 # embeddings_queue_name permissions
 
-resource "azurerm_role_assignment" "sb_embeddings_queue_contributor_embeddings_dispatcher_func" {
+resource "azurerm_role_assignment" "embeddings_dispatcher_func_sb_data_receiver_on_embeddings_queue" {
   principal_id         = module.image_handler_func.function_app_identity.principal_id
   scope                = module.embeddings_queue.queues[var.embeddings_queue_name].id
   role_definition_name = "Azure Service Bus Data Receiver"
