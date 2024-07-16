8b2536708c1825eabe73d14f0edb1527b56fca99
SirNatan1 <natan757@gmail.com>
2024-07-16 21:51:12 +0300

    Added: backend to gitignore resource for custome storage

--- azure/terraform/native/locals-storage_account.tf
+++ azure/terraform/native/locals-storage_account.tf
@@ -1,18 +1,10 @@
 locals {
   model_container_name = "model"
 
-
-  use_custom_resource_group  = var.image_storage_account_resource_group_name == null ? false : true
-  use_custom_storage_account = var.images_storage_account_name == null ? false : true
-
   image_containers = [
     {
       name = var.images_container_name
     }
   ]
 
-  image_container_exist = contains(
-    try(data.azurerm_storage_containers.image_storage_account_containers.0.containers.*.name, []),
-    var.images_container_name
-  )
 }
--- azure/terraform/native/resources-storage_account.tf
+++ azure/terraform/native/resources-storage_account.tf
@@ -89,9 +89,4 @@ moved {
   to   = module.image_uploader_storage_account[0]
 }
 
-resource "azurerm_storage_container" "custom_image_container" {
-  count                 = !local.use_custom_storage_account || local.image_container_exist ? 0 : 1
-  name                  = var.images_container_name
-  storage_account_name  = data.azurerm_storage_account.custom_storage_account.0.name
-  container_access_type = "private"
-}
+
