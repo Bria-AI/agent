33bf8e5d679d7146bbb9a00f297640b69fd1b8ec
Orm <or.moran@kornit.com>
2024-07-16 20:04:38 +0300

    Fix storage

--- azure/terraform/native/data-storage_account.tf
+++ azure/terraform/native/data-storage_account.tf
@@ -1,7 +1,7 @@
 data "azurerm_storage_account" "custom_storage_account" {
   count               = local.use_custom_storage_account && local.use_custom_resource_group ? 1 : 0
   name                = var.images_storage_account_name
-  resource_group_name = var.storage_account_resource_group_name
+  resource_group_name = var.image_storage_account_resource_group_name
 }
 
 data "azurerm_storage_containers" "image_storage_account_containers" {
--- azure/terraform/native/locals-storage_account.tf
+++ azure/terraform/native/locals-storage_account.tf
@@ -2,7 +2,7 @@ locals {
   model_container_name = "model"
 
 
-  use_custom_resource_group  = var.storage_account_resource_group_name == null ? false : true
+  use_custom_resource_group  = var.image_storage_account_resource_group_name == null ? false : true
   use_custom_storage_account = var.images_storage_account_name == null ? false : true
 
   image_containers = [
